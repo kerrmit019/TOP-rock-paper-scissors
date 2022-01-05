fa6a1b466ec7c07bf207a7ca30658eb600e5bec7
Sarah Kerr <sarah.kerr2@hotmail.com>
2022-01-05 21:19:45 +1300

    Complete button click with output to console

--- script.js
+++ script.js
@@ -12,15 +12,15 @@ let computerScore = 0;
 // run the game
 // game();
 
-// let player input their selection [rock, paper, scissors] for the round
-function playerMove() {
-  // let playerMove = prompt("What's your move (Rock, Paper, or Scissors)?");
-  let playerMove = "rock";
-  if (playerMove === null) {
-    return "Move skipped.";
-  }
-  return playerMove;
-}
+// // let player input their selection [rock, paper, scissors] for the round
+// function playerMove() {
+//   // let playerMove = prompt("What's your move (Rock, Paper, or Scissors)?");
+//   let playerMove = "rock";
+//   if (playerMove === null) {
+//     return "Move skipped.";
+//   }
+//   return playerMove;
+// }
 
 //  select between rock, paper and scissors (computer's move)
 function computerPlay() {
@@ -38,13 +38,27 @@ function updateScore(winner) {
   //   console.log(playerScore, computerScore);
 }
 
-function calculateGameWinner() {
-  if (playerScore === computerScore) {
-    return "The game is a draw!";
-  } else if (playerScore > computerScore) {
-    return "You win the game!";
+// replaced with checkForWinner in update of the game
+// function calculateGameWinner() {
+//   if (playerScore === computerScore) {
+//     return "The game is a draw!";
+//   } else if (playerScore > computerScore) {
+//     return "You win the game!";
+//   } else {
+//     return "You Lose the game!";
+//   }
+// }
+
+// First to 5 points wins
+function checkForGameWinner() {
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore > computerScore) {
+      return "You win the game!";
+    } else {
+      return "You Lose the game!";
+    }
   } else {
-    return "You Lose the game!";
+    return "Choose again";
   }
 }
 
@@ -53,7 +67,7 @@ function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   //   player wins round
-  function winner(playerSelection, computerSelection) {
+  function playerWinner(playerSelection, computerSelection) {
     updateScore("player");
     return `You Win! ${
       playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1)
@@ -61,7 +75,7 @@ function playRound(playerSelection, computerSelection) {
   }
 
   //   player loses round--- pc wins
-  function loser(playerSelection, computerSelection) {
+  function playerLoser(playerSelection, computerSelection) {
     updateScore("computer");
     return `You Lose! ${computerSelection} beats ${
       playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1)
@@ -77,23 +91,23 @@ function playRound(playerSelection, computerSelection) {
   switch (playerSelection) {
     case "rock":
       if (computerSelection === "Scissors") {
-        return winner(playerSelection, computerSelection);
+        return playerWinner(playerSelection, computerSelection);
       } else {
-        return loser(playerSelection, computerSelection);
+        return playerLoser(playerSelection, computerSelection);
       }
 
     case "paper":
       if (computerSelection === "Rock") {
-        return winner(playerSelection, computerSelection);
+        return playerWinner(playerSelection, computerSelection);
       } else {
-        return loser(playerSelection, computerSelection);
+        return playerLoser(playerSelection, computerSelection);
       }
 
     case "scissors":
       if (computerSelection === "Paper") {
-        return winner(playerSelection, computerSelection);
+        return playerWinner(playerSelection, computerSelection);
       } else {
-        return loser(playerSelection, computerSelection);
+        return playerLoser(playerSelection, computerSelection);
       }
 
     default:
@@ -104,9 +118,9 @@ function playRound(playerSelection, computerSelection) {
 // keep score and report at end
 // announce the winner
 function game() {
-  console.log(playRound(playerMove(), computerPlay()));
+  // console.log(playRound(playerMove(), computerPlay()));
 
-  console.log(`Player: ${playerScore}; Computer: ${computerScore}`);
+  // console.log(`Player: ${playerScore}; Computer: ${computerScore}`);
   console.log(calculateGameWinner());
   return;
 }
@@ -118,9 +132,15 @@ const buttons = document.querySelectorAll("button");
 // add click event listener for each button, which returns the label of the
 // button - Rock, Paper or Scissors, to the playRound function
 buttons.forEach((button) =>
-  button.addEventListener("click", (e) =>
-    console.log(playRound(e.target.textContent, computerPlay()))
-  )
+  button.addEventListener("click", (e) => {
+    const roundOutcome = playRound(e.target.textContent, computerPlay());
+    console.log(roundOutcome);
+
+    console.log(`Player: ${playerScore}; Computer: ${computerScore}`);
+
+    // check for a winner
+    console.log(checkForGameWinner());
+  })
 );
 
 // TODOS
@@ -136,3 +156,6 @@ buttons.forEach((button) =>
 // That’s OK! Reworking old code is an important part of a programmer’s life.
 // Once you’re all done with your UI and make sure everything’s satisfactory,
 // it’s time to merge our current branch rps-ui to our main branch.
+
+// My Extra task for me...
+// Also add a score reset once at 5 pts (i.e. game over) with button pop up to play again?
